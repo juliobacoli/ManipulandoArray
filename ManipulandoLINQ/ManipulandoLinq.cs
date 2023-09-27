57b2be1ccedefcba72f79d4ce823f5917c03c850
Julio Bacoli <julio-microlins@hotmail.com>
2023-09-27 02:03:46 -0300

    Preparando ambiente para teste com LINQ

---
+++ ManipulandoLINQ/ManipulandoLinq.cs
@@ -0,0 +1,18 @@
+﻿namespace ManipulandoLINQ;
+
+public class ManipulandoLinq
+{
+    public static void Main(string[] args)
+    {
+        var students = new List<Student>
+        {
+            new Student(1, "Julio", "102030", 100),
+            new Student(2, "Joao", "112030", 200),
+            new Student(3, "Paulo", "122030", 300),
+            new Student(4, "Jose", "132030", 400),
+            new Student(5, "Felipe", "142030", 500),
+            new Student(6, "Alice", "152030", 600),
+            new Student(7, "Bianka", "162030", 700)
+        };
+    }
+}
---
+++ ManipulandoLINQ/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ManipulandoLINQ;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Matricula { get; set; }
+    public int Notas { get; set; }
+
+    public Student(int id, string nome, string matricula, int notas)
+    {
+        Id = id;
+        Nome = nome;
+        Matricula = matricula;
+        Notas = notas;
+    }
+}
